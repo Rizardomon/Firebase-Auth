fdf42cb46e93d2ac6585bf76e4d8831ebfd0f8ef
Richard Robinson Campos <rrobinson.campos@gmail.com>
2021-01-15 12:53:48 -0300

    Lesson #3

---
+++ lib/screens/wrapper.dart
@@ -0,0 +1,10 @@
+import 'package:firebase_authentication/screens/authenticate/authenticate.dart';
+import 'package:firebase_authentication/screens/home/home.dart';
+import 'package:flutter/material.dart';
+
+class Wrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Authenticate();
+  }
+}
