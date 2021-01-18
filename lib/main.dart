543070896b2b76e161efce4f88ccba2b768f3e4b
Richard Robinson Campos <rrobinson.campos@gmail.com>
2021-01-18 10:23:02 -0300

    Lesson #8 - Provider Package

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
+import 'package:firebase_authentication/models/user.dart';
 import 'package:firebase_authentication/screens/wrapper.dart';
+import 'package:firebase_authentication/services/auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:provider/provider.dart';
 
 main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -11,8 +14,11 @@ main() async {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Wrapper(),
+    return StreamProvider<UserModel>.value(
+      value: AuthService().user,
+      child: MaterialApp(
+        home: Wrapper(),
+      ),
     );
   }
 }
--- lib/screens/wrapper.dart
+++ lib/screens/wrapper.dart
@@ -1,10 +1,14 @@
+import 'package:firebase_authentication/models/user.dart';
 import 'package:firebase_authentication/screens/authenticate/authenticate.dart';
 import 'package:firebase_authentication/screens/home/home.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class Wrapper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final user = Provider.of<UserModel>(context);
+    print(user);
     return Authenticate();
   }
 }
