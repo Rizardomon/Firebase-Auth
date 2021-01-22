6632589607f49b5b9647a3b4b7f71597a35f8355
Richard Robinson Campos <rrobinson.campos@gmail.com>
2021-01-22 14:35:21 -0300

    Lesson #25 - Using a Stream Builder

--- lib/screens/home/settings_form.dart
+++ lib/screens/home/settings_form.dart
@@ -1,4 +1,8 @@
+import 'package:firebase_authentication/models/user.dart';
+import 'package:firebase_authentication/services/database.dart';
+import 'package:firebase_authentication/shared/loading.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class SettingsForm extends StatefulWidget {
   @override
@@ -15,68 +19,83 @@ class _SettingsFormState extends State<SettingsForm> {
 
   @override
   Widget build(BuildContext context) {
-    return Form(
-      key: _formKey,
-      child: Column(
-        children: [
-          Text(
-            'Update your brew settings',
-            style: TextStyle(
-              fontSize: 18,
-            ),
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          TextFormField(
-            decoration: InputDecoration(
-              labelText: 'Update yor name',
-              border: OutlineInputBorder(),
-            ),
-            validator: (val) => val.isEmpty ? 'Please enter a name' : null,
-            onChanged: (val) => setState(() => _currentName = val),
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          // Dropdown
-          DropdownButtonFormField(
-            decoration: InputDecoration(
-              border: OutlineInputBorder(),
-            ),
-            value: _currentSugars ?? '0',
-            items: sugars.map((sugar) {
-              return DropdownMenuItem(
-                value: sugar,
-                child: Text('$sugar sugars'),
-              );
-            }).toList(),
-            onChanged: (val) => setState(() => _currentSugars = val),
-          ),
-          // Slider
-          Slider(
-            min: 100,
-            max: 900,
-            divisions: 8,
-            onChanged: (val) => setState(() => _currentStrength = val.round()),
-            value: (_currentStrength ?? 100).toDouble(),
-            activeColor: Colors.brown[_currentStrength ?? 100],
-            inactiveColor: Colors.brown[_currentStrength ?? 100],
-          ),
-          RaisedButton(
-            color: Colors.black,
-            onPressed: () async {
-              print(_currentName);
-              print(_currentSugars);
-              print(_currentStrength);
-            },
-            child: Text(
-              'Update',
-              style: TextStyle(color: Colors.white),
-            ),
-          )
-        ],
-      ),
-    );
+    final user = Provider.of<UserModel>(context);
+    return StreamBuilder<UserData>(
+        stream: DatabaseService(uid: user.uid).userData,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            UserData userData = snapshot.data;
+            return Form(
+              key: _formKey,
+              child: Column(
+                children: [
+                  Text(
+                    'Update your brew settings',
+                    style: TextStyle(
+                      fontSize: 18,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  TextFormField(
+                    initialValue: userData.name,
+                    decoration: InputDecoration(
+                      labelText: 'Update yor name',
+                      border: OutlineInputBorder(),
+                    ),
+                    validator: (val) =>
+                        val.isEmpty ? 'Please enter a name' : null,
+                    onChanged: (val) => setState(() => _currentName = val),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  // Dropdown
+                  DropdownButtonFormField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                    ),
+                    value: _currentSugars ?? userData.sugars,
+                    items: sugars.map((sugar) {
+                      return DropdownMenuItem(
+                        value: sugar,
+                        child: Text('$sugar sugars'),
+                      );
+                    }).toList(),
+                    onChanged: (val) => setState(() => _currentSugars = val),
+                  ),
+                  // Slider
+                  Slider(
+                    min: 100,
+                    max: 900,
+                    divisions: 8,
+                    onChanged: (val) =>
+                        setState(() => _currentStrength = val.round()),
+                    value: (_currentStrength ?? userData.strength).toDouble(),
+                    activeColor:
+                        Colors.brown[_currentStrength ?? userData.strength],
+                    inactiveColor:
+                        Colors.brown[_currentStrength ?? userData.strength],
+                  ),
+                  RaisedButton(
+                    color: Colors.black,
+                    onPressed: () async {
+                      print(_currentName);
+                      print(_currentSugars);
+                      print(_currentStrength);
+                    },
+                    child: Text(
+                      'Update',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  )
+                ],
+              ),
+            );
+          } else {
+            return Loading();
+          }
+        });
   }
 }
