87919fe2f9a8e09790e85815b81a397042bdd4e7
Richard Robinson Campos <rrobinson.campos@gmail.com>
2021-01-22 13:38:55 -0300

    Lesson #20 - Listing Brew Data

---
+++ lib/screens/home/brew_tile.dart
@@ -0,0 +1,26 @@
+import 'package:firebase_authentication/models/brew.dart';
+import 'package:flutter/material.dart';
+
+class BrewTile extends StatelessWidget {
+  final Brew brew;
+
+  const BrewTile({Key key, this.brew}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 8.0),
+      child: Card(
+        margin: EdgeInsets.fromLTRB(20, 6, 20, 0),
+        child: ListTile(
+          leading: CircleAvatar(
+            radius: 25,
+            backgroundColor: Colors.brown[brew.strength],
+          ),
+          title: Text(brew.name),
+          subtitle: Text('Takes ${brew.sugars} sugar(s)'),
+        ),
+      ),
+    );
+  }
+}
