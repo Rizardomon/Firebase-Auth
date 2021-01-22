6dd234f5ac7d8bf19fe4062e4bdbe50140355e1f
Richard Robinson Campos <rrobinson.campos@gmail.com>
2021-01-22 09:53:29 -0300

    Lesson #15 - Loading Widget

--- lib/screens/authenticate/register.dart
+++ lib/screens/authenticate/register.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_authentication/services/auth.dart';
+import 'package:firebase_authentication/shared/loading.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
@@ -14,6 +15,7 @@ class Register extends StatefulWidget {
 class _RegisterState extends State<Register> {
   final AuthService _auth = AuthService();
   final _formKey = GlobalKey<FormState>();
+  bool loading = false;
 
   String email = '';
   String password = '';
@@ -21,108 +23,113 @@ class _RegisterState extends State<Register> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.brown[100],
-      appBar: AppBar(
-        backgroundColor: Colors.brown[400],
-        elevation: 0,
-        title: Text('Register'),
-        actions: [
-          FlatButton.icon(
-            icon: Icon(Icons.person),
-            label: Text('Sign in'),
-            onPressed: () {
-              widget.toggleView();
-            },
-          )
-        ],
-      ),
-      body: Container(
-        padding: EdgeInsets.symmetric(vertical: 20, horizontal: 30),
-
-        child: Form(
-            key: _formKey,
-            child: Column(
-              children: [
-                SizedBox(
-                  height: 20,
-                ),
-                TextFormField(
-                  decoration: InputDecoration(
-                    hintText: 'Email',
-                    filled: true,
-                    fillColor: Colors.white,
-                  ),
-                  validator: (value) => value.isEmpty ? 'Enter an email' : null,
-                  onChanged: (value) {
-                    setState(() {
-                      return email = value;
-                    });
+    return loading
+        ? Loading()
+        : Scaffold(
+            backgroundColor: Colors.brown[100],
+            appBar: AppBar(
+              backgroundColor: Colors.brown[400],
+              elevation: 0,
+              title: Text('Register'),
+              actions: [
+                FlatButton.icon(
+                  icon: Icon(Icons.person),
+                  label: Text('Sign in'),
+                  onPressed: () {
+                    widget.toggleView();
                   },
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                TextFormField(
-                  decoration: InputDecoration(
-                    hintText: 'Password',
-                    filled: true,
-                    fillColor: Colors.white,
-                  ),
-                  validator: (value) => value.length < 6
-                      ? 'Enter a password 6+ chars long'
-                      : null,
-                  onChanged: (value) {
-                    setState(() {
-                      return password = value;
-                    });
-                  },
-                  obscureText: true,
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                RaisedButton(
-                  onPressed: () async {
-                    WidgetsFlutterBinding.ensureInitialized();
-                    await Firebase.initializeApp();
-                    if (_formKey.currentState.validate()) {
-                      dynamic result = await _auth.registerWithEmailAndPassword(
-                          email, password);
-                      if (result == null) {
-                        setState(() {
-                          return error = 'Please suply a valid email';
-                        });
-                      }
-                    } else {}
-                  },
-                  color: Colors.black,
-                  child: Text(
-                    'Register',
-                    style: TextStyle(color: Colors.white),
-                  ),
-                ),
-                SizedBox(
-                  height: 12,
-                  child: Text(error, style: TextStyle(color: Colors.red)),
-                ),
+                )
               ],
-            )),
-        // child: RaisedButton(
-        //   onPressed: () async {
-        //     WidgetsFlutterBinding.ensureInitialized();
-        //     await Firebase.initializeApp();
-        //     dynamic result = await _auth.signInAnon();
-        //     if (result == null) {
-        //       print('error signing in');
-        //     } else {
-        //       print('Signed');
-        //       print(result.uid);
-        //     }
-        //   },
-        //   child: Text('Sign In Anon'),
-        // ),
-      ),
-    );
+            ),
+            body: Container(
+              padding: EdgeInsets.symmetric(vertical: 20, horizontal: 30),
+
+              child: Form(
+                  key: _formKey,
+                  child: Column(
+                    children: [
+                      SizedBox(
+                        height: 20,
+                      ),
+                      TextFormField(
+                        decoration: InputDecoration(
+                          hintText: 'Email',
+                          filled: true,
+                          fillColor: Colors.white,
+                        ),
+                        validator: (value) =>
+                            value.isEmpty ? 'Enter an email' : null,
+                        onChanged: (value) {
+                          setState(() {
+                            return email = value;
+                          });
+                        },
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      TextFormField(
+                        decoration: InputDecoration(
+                          hintText: 'Password',
+                          filled: true,
+                          fillColor: Colors.white,
+                        ),
+                        validator: (value) => value.length < 6
+                            ? 'Enter a password 6+ chars long'
+                            : null,
+                        onChanged: (value) {
+                          setState(() {
+                            return password = value;
+                          });
+                        },
+                        obscureText: true,
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      RaisedButton(
+                        onPressed: () async {
+                          WidgetsFlutterBinding.ensureInitialized();
+                          await Firebase.initializeApp();
+                          if (_formKey.currentState.validate()) {
+                            setState(() => loading = true);
+                            dynamic result = await _auth
+                                .registerWithEmailAndPassword(email, password);
+                            if (result == null) {
+                              setState(() {
+                                error = 'Please suply a valid email';
+                                loading = false;
+                              });
+                            }
+                          } else {}
+                        },
+                        color: Colors.black,
+                        child: Text(
+                          'Register',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 12,
+                        child: Text(error, style: TextStyle(color: Colors.red)),
+                      ),
+                    ],
+                  )),
+              // child: RaisedButton(
+              //   onPressed: () async {
+              //     WidgetsFlutterBinding.ensureInitialized();
+              //     await Firebase.initializeApp();
+              //     dynamic result = await _auth.signInAnon();
+              //     if (result == null) {
+              //       print('error signing in');
+              //     } else {
+              //       print('Signed');
+              //       print(result.uid);
+              //     }
+              //   },
+              //   child: Text('Sign In Anon'),
+              // ),
+            ),
+          );
   }
 }
--- lib/screens/authenticate/sign_in.dart
+++ lib/screens/authenticate/sign_in.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_authentication/services/auth.dart';
+import 'package:firebase_authentication/shared/loading.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 
@@ -13,6 +14,7 @@ class SignIn extends StatefulWidget {
 class _SignInState extends State<SignIn> {
   final AuthService _auth = AuthService();
   final _formKey = GlobalKey<FormState>();
+  bool loading = false;
 
   // Text field state
   String email = '';
@@ -21,108 +23,114 @@ class _SignInState extends State<SignIn> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.brown[100],
-      appBar: AppBar(
-        backgroundColor: Colors.brown[400],
-        elevation: 0,
-        title: Text('Sign In'),
-        actions: [
-          FlatButton.icon(
-            icon: Icon(Icons.person),
-            label: Text('Register'),
-            onPressed: () {
-              widget.toggleView();
-            },
-          )
-        ],
-      ),
-      body: Container(
-        padding: EdgeInsets.symmetric(vertical: 20, horizontal: 30),
-        child: Form(
-            key: _formKey,
-            child: Column(
-              children: [
-                SizedBox(
-                  height: 20,
-                ),
-                TextFormField(
-                  decoration: InputDecoration(
-                    hintText: 'Email',
-                    filled: true,
-                    fillColor: Colors.white,
-                  ),
-                  validator: (value) => value.isEmpty ? 'Enter an email' : null,
-                  onChanged: (value) {
-                    setState(() {
-                      return email = value;
-                    });
+    return loading
+        ? Loading()
+        : Scaffold(
+            backgroundColor: Colors.brown[100],
+            appBar: AppBar(
+              backgroundColor: Colors.brown[400],
+              elevation: 0,
+              title: Text('Sign In'),
+              actions: [
+                FlatButton.icon(
+                  icon: Icon(Icons.person),
+                  label: Text('Register'),
+                  onPressed: () {
+                    widget.toggleView();
                   },
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                TextFormField(
-                  decoration: InputDecoration(
-                    hintText: 'Password',
-                    filled: true,
-                    fillColor: Colors.white,
-                  ),
-                  validator: (value) => value.length < 6
-                      ? 'Enter a password 6+ chars long'
-                      : null,
-                  onChanged: (value) {
-                    setState(() {
-                      return password = value;
-                    });
-                  },
-                  obscureText: true,
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                RaisedButton(
-                  onPressed: () async {
-                    WidgetsFlutterBinding.ensureInitialized();
-                    await Firebase.initializeApp();
-                    if (_formKey.currentState.validate()) {
-                      dynamic result = await _auth.signInWithEmailAndPassword(
-                          email, password);
-                      if (result == null) {
-                        setState(() {
-                          return error =
-                              'Please suply a valid email and password';
-                        });
-                      }
-                    }
-                  },
-                  color: Colors.black,
-                  child: Text(
-                    'Sign in',
-                    style: TextStyle(color: Colors.white),
-                  ),
-                ),
-                SizedBox(
-                  height: 12,
-                  child: Text(error, style: TextStyle(color: Colors.red)),
-                ),
+                )
               ],
-            )),
-        // child: RaisedButton(
-        //   onPressed: () async {
-        //     WidgetsFlutterBinding.ensureInitialized();
-        //     await Firebase.initializeApp();
-        //     dynamic result = await _auth.signInAnon();
-        //     if (result == null) {
-        //       print('error signing in');
-        //     } else {
-        //       print('Signed');
-        //       print(result.uid);
-        //     }
-        //   },
-        //   child: Text('Sign In Anon'),
-        // ),
-      ),
-    );
+            ),
+            body: Container(
+              padding: EdgeInsets.symmetric(vertical: 20, horizontal: 30),
+              child: Form(
+                  key: _formKey,
+                  child: Column(
+                    children: [
+                      SizedBox(
+                        height: 20,
+                      ),
+                      TextFormField(
+                        decoration: InputDecoration(
+                          hintText: 'Email',
+                          filled: true,
+                          fillColor: Colors.white,
+                        ),
+                        validator: (value) =>
+                            value.isEmpty ? 'Enter an email' : null,
+                        onChanged: (value) {
+                          setState(() {
+                            return email = value;
+                          });
+                        },
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      TextFormField(
+                        decoration: InputDecoration(
+                          hintText: 'Password',
+                          filled: true,
+                          fillColor: Colors.white,
+                        ),
+                        validator: (value) => value.length < 6
+                            ? 'Enter a password 6+ chars long'
+                            : null,
+                        onChanged: (value) {
+                          setState(() {
+                            return password = value;
+                          });
+                        },
+                        obscureText: true,
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      RaisedButton(
+                        onPressed: () async {
+                          WidgetsFlutterBinding.ensureInitialized();
+                          await Firebase.initializeApp();
+                          if (_formKey.currentState.validate()) {
+                            setState(() => loading = true);
+                            dynamic result = await _auth
+                                .signInWithEmailAndPassword(email, password);
+                            if (result == null) {
+                              setState(() {
+                                error =
+                                    'Please suply a valid email and password';
+
+                                loading = false;
+                              });
+                            }
+                          }
+                        },
+                        color: Colors.black,
+                        child: Text(
+                          'Sign in',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 12,
+                        child: Text(error, style: TextStyle(color: Colors.red)),
+                      ),
+                    ],
+                  )),
+              // child: RaisedButton(
+              //   onPressed: () async {
+              //     WidgetsFlutterBinding.ensureInitialized();
+              //     await Firebase.initializeApp();
+              //     dynamic result = await _auth.signInAnon();
+              //     if (result == null) {
+              //       print('error signing in');
+              //     } else {
+              //       print('Signed');
+              //       print(result.uid);
+              //     }
+              //   },
+              //   child: Text('Sign In Anon'),
+              // ),
+            ),
+          );
   }
 }
